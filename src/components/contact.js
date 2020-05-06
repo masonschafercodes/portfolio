c0ee6018b856b9d8fdb2fb3ced4623d3d3556147
Mason Schafer <mason.schafer1@gmail.com>
2020-05-05 21:08:29 -0400

    adding contact form actions

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import  {Button} from 'reactstrap';
 import styled, {keyframes} from 'styled-components';
 import {bounce} from 'react-animations';
-import bounceIn from 'react-animations/lib/bounce-in';
+import bounceIn from 'react-animations/lib/bounce-in'
 
 const bounceAnimation = keyframes`${bounce}`;
 
@@ -11,23 +11,57 @@ const BouncyDiv = styled.div`animation: 1s ${bounceAnimation};`
 const bounceInAnimation = keyframes`${bounceIn}`;
 const BounceInDiv = styled.div`animation: 1.5s ${bounceInAnimation};`
 
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
+
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: "", email: "", message: "" };
+      }
+
+      handleSubmit = e => {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", ...this.state })
+        })
+          .then(() => alert("Success!"))
+          .catch(error => alert(error));
+
+        e.preventDefault();
+      };
+
+      handleChange = e => this.setState({ [e.target.name]: e.target.value });
     
     render(){
-        return(
-
+        const { name, email, message } = this.state;
+        return(   
 
             <div className="contact-body" style={{textAlign: 'center'}}>
 
                 <BounceInDiv><h1 style={{textAlign: 'center'}}>Let's Talk.</h1></BounceInDiv>
                 <h5 style={{textAlign: 'center'}}>If you have any questions about my projects or any other random questions, why not just shoot me an email.</h5>
-                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
                     <input type="hidden" name="contact" value="contact" />
                     <p>
-                        <label>Name <input type="text" name="name" /></label>
+                        <label>
+                            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+                        </label>
                     </p>
                     <p>
-                        <label>Email <input type="email" name="email" /></label>
+                        <label>
+                            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Message: <textarea name="message" value={message} onChange={this.handleChange} />
+                        </label>
                     </p>
                     <p>
                         <Button type="submit" color="primary">Send</Button>
@@ -37,7 +71,6 @@ class Contact extends Component {
             </div>
         );
     }
-    // 6B073CBF58B96BEC462E96EF2FB28CEB3C18
 }
 
 
