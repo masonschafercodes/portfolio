a5b8f279e1c17868789249dbcb93a07277e9825e
Mason Schafer <mason.schafer1@gmail.com>
2020-05-05 20:48:17 -0400

    adding contact form actions

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 import  {Button} from 'reactstrap';
+import styled, {keyframes} from 'styled-components';
+import {bounce} from 'react-animations';
+import bounceIn from 'react-animations/lib/bounce-in';
+
+const bounceAnimation = keyframes`${bounce}`;
+
+const BouncyDiv = styled.div`animation: 1s ${bounceAnimation};`
+
+const bounceInAnimation = keyframes`${bounceIn}`;
+const BounceInDiv = styled.div`animation: 1.5s ${bounceInAnimation};`
 
 class Contact extends Component {
     
@@ -9,9 +19,19 @@ class Contact extends Component {
 
             <div className="contact-body" style={{textAlign: 'center'}}>
 
-                <h1 style={{textAlign: 'center'}}>Let's Talk.</h1>
+                <BounceInDiv><h1 style={{textAlign: 'center'}}>Let's Talk.</h1></BounceInDiv>
                 <h5 style={{textAlign: 'center'}}>If you have any questions about my projects or any other random questions, why not just shoot me an email.</h5>
-                <a href="mailto:mason.schafer1@gmail.com"><Button color="primary" style={{width: '155px', height: '53px'}}>Contact Me!</Button></a>
+                <form name="contact" method="POST" data-netlify="true">
+                    <p>
+                        <label>Name <input type="text" name="name" /></label>
+                    </p>
+                    <p>
+                        <label>Email <input type="email" name="email" /></label>
+                    </p>
+                    <p>
+                        <Button type="submit" color="primary">Send</Button>
+                    </p>
+                </form>
 
             </div>
         );
