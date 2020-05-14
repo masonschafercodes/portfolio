3a6ecebbfdf690fe35d7d966d2258cdb8eff307b
Mason Schafer <mason.schafer1@gmail.com>
2020-05-14 16:33:45 -0400

    asdasd

--- src/components/contact.js
+++ src/components/contact.js
@@ -23,13 +23,6 @@ class Contact extends Component {
             var email = { email };
             var message = { message };
 
-            if (this.state.email === '' && this.state.message === ''){
-                return (
-                    document.getElementById("completed").innerHTML = "Not Sent",
-                    document.getElementById("completed").style.color = 'red'
-                    )
-            }
-
             await fetch("https://masonschaferapi.xyz/api/email", {
                 method: 'POST',
                 headers: {
@@ -39,7 +32,7 @@ class Contact extends Component {
                 body: JSON.stringify({'from': this.state.email, 'text': this.state.message})
             }).then(
                 document.getElementById("completed").innerHTML = "Completed",
-                document.getElementById("completed").style.color = 'green'
+                document.getElementById("completed").style.color = 'green'  
             );
 
 
@@ -56,11 +49,11 @@ class Contact extends Component {
                <Form style={{marginTop: '15px'}} onSubmit={this.onSumbitForm.bind(this)}>
                    <FormGroup>
                        <Label>Email: </Label>
-                       <Input type="email" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange.bind(this)} />
+                       <Input type="email" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange.bind(this)} required/>
                    </FormGroup>
                    <FormGroup>
                        <Label>Message: </Label>
-                       <Input type="textarea" placeholder="Message to Me" name="message" value={this.state.message} onChange={this.handleChange.bind(this)} />
+                       <Input type="textarea" placeholder="Message to Me" name="message" value={this.state.message} onChange={this.handleChange.bind(this)} required/>
                    </FormGroup>
                    <Button color="primary" style={{marginTop: '15px'}} type="submit">Send</Button>
                </Form>
