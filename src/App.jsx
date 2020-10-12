4c038b76aebd5303da9da19e9eccf6d43b9170e0
masonschafercodes <mason.schafer1@gmail.com>
2020-10-12 10:41:31 -0400

    changing how things are layed out and added some links to my socials

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,112 @@
-import React from 'react';
+import React from "react";
 import { Fade } from "react-awesome-reveal";
-import './App.css';
-import ThingsILove from './Components/ThingsILove';
+import "./App.css";
+import ThingsILove from "./Components/ThingsILove";
 
 function App() {
   return (
     <Fade>
-    <div className="App">
-      <div className="nav-container">
-        <a href="/projects" className="font-main"><p>Projects</p><br/></a>
-        <a href="/blog" className="font-main"><p>Blog</p></a>
-      </div>
-      <div className="main-container">
-        <img src="https://i.ibb.co/7kPQGVT/icon.png" alt="Mason Schafer Portfolio" />
-        <h1 className="font-main color1">I am <span className="hash-color">Mason Schafer</span></h1>
-        <div className="about-me">
-          <h3 className="font-main color1">About me.</h3>
-          <div className="whoami">
-            <p className="font-main color1"><span className="hash-color">#</span> 19 years old</p>
-            <p className="font-main color1"><span className="hash-color">#</span> Creative and Motivated</p>
-            <p className="font-main color1"><span className="hash-color">#</span> Design-Oriented</p>
-          </div>
+      <div className="App">
+        <div className="nav-container">
+          <a href="/projects" className="font-main">
+            <p>Projects</p>
+            <br />
+          </a>
+          <a href="/blog" className="font-main">
+            <p>Blog</p>
+          </a>
+          <a href="https://github.com/masonschafercodes" target="_blank" rel="noopener noreferrer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+            </svg>
+          </a>
+          <a href="https://www.linkedin.com/in/masonschafer" target="_blank" rel="noopener noreferrer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+              <rect x="2" y="9" width="4" height="12"></rect>
+              <circle cx="4" cy="4" r="2"></circle>
+            </svg>
+          </a>
         </div>
-        <ThingsILove />
-      </div>
-      <footer>
-        <div className="footer-container">
-          <div className="socials-container">
-            <a href="https://github.com/masonschafercodes" rel="noopener noreferrer" target="_blank"><p className="font-main color1"><span className="hash-color">#</span> GitHub</p></a>
-            <a href="https://linkedin.com/in/masonschafer/" rel="noopener noreferrer" target="_blank"><p className="font-main color1"><span className="hash-color">#</span> LinkedIn</p></a>
+        <div className="main-container">
+          <img
+            src="https://i.ibb.co/7kPQGVT/icon.png"
+            alt="Mason Schafer Portfolio"
+          />
+          <h1 className="font-main color1">
+            I am <span className="hash-color">Mason Schafer</span>
+          </h1>
+          <div className="about-me">
+            <h3 className="font-main color1">About me.</h3>
+            <div className="whoami">
+              <p className="font-main color1">
+                <span className="hash-color">#</span> 19 years old
+              </p>
+              <p className="font-main color1">
+                <span className="hash-color">#</span> Creative and Motivated
+              </p>
+              <p className="font-main color1">
+                <span className="hash-color">#</span> Design-Oriented
+              </p>
+            </div>
           </div>
-          <div className="cpyright-container">
-          <p className="font-main color1">2020 Mason Schafer</p>
-          </div>
+          <ThingsILove
+            url={[
+              "https://i.ibb.co/25z7WG9/pexels-adrianna-calvo-57627.jpg",
+              "https://i.ibb.co/1673C48/pexels-kevin-ku-577585.jpg",
+              "https://i.ibb.co/PjBPSCN/pexels-tim-gouw-139762.jpg",
+              "https://i.ibb.co/VtVck8w/pexels-evie-shaffer-2923672.jpg",
+            ]}
+          />
         </div>
-      </footer>
-    </div>
+        <footer>
+          <div className="footer-container">
+            <div className="socials-container">
+              <a
+                href="https://github.com/masonschafercodes"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <p className="font-main color1">
+                  <span className="hash-color">#</span> GitHub
+                </p>
+              </a>
+              <a
+                href="https://linkedin.com/in/masonschafer/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <p className="font-main color1">
+                  <span className="hash-color">#</span> LinkedIn
+                </p>
+              </a>
+            </div>
+            <div className="cpyright-container">
+              <p className="font-main color1">2020 Mason Schafer</p>
+            </div>
+          </div>
+        </footer>
+      </div>
     </Fade>
   );
 }
--- src/Components/ThingsILove.jsx
+++ src/Components/ThingsILove.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import "../App.css";
 
-const ThingsILove = () => {
+const ThingsILove = (props) => {
   return (
     <div className="things-I-love">
       <h1 className="font-main color1">Things I Love: </h1>
       <div>
-        <img
-          src="https://i.ibb.co/25z7WG9/pexels-adrianna-calvo-57627.jpg"
-          alt="of a dog"
-        />
-        <img
-          src="https://i.ibb.co/1673C48/pexels-kevin-ku-577585.jpg"
-          alt="computer science"
-        />
-        <img
-          src="https://i.ibb.co/PjBPSCN/pexels-tim-gouw-139762.jpg"
-          alt="baseball diamond"
-        />
+        {props.url.map(link => (
+          <img src={link} key={link} alt="things that I love" />
+        ))
+        }
       </div>
     </div>
   );
